d3d3e3bf1322cff6bb4bf8578fe5fff10ba29794
Alexander Stanevko <dardawill@yandex.ru>
2024-01-22 14:25:14 +0300

    Add workflow

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,7 +8,7 @@ import HelloWorld from '../components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Test Message" />
+      <HelloWorld msg="Test Message With Workflow" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
