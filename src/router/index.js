64c39283e64c320585c68d43217b990b2fbb65cb
Alexander Stanevko <dardawill@yandex.ru>
2024-01-22 13:26:52 +0300

    Add error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainLayout from '../layouts/MainLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue')
+        },
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/:catchAll(.*)*',
+      name: 'ErrorPage',
+      component: () => import('../views/ErrorNotFound.vue'),
+    },
   ]
 })
 
-export default router
+export default router;
---
+++ src/views/ErrorNotFound.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="erroe-page q-pa-md flex flex-center">
+      <div>
+        <div class="h1">
+          404
+        </div>
+  
+        <div class="h2">
+          Страница не найдена
+        </div>
+  
+        <RouterLink to="/">Home</RouterLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style>
+  .erroe-page {
+    color: black;
+    font-size: 48px;
+    text-align: center;
+  }
+  .fullscreen {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .text-center {
+    text-align: center;
+  }
+  
+  .flex {
+    display: flex;
+  }
+  
+  .flex-center {
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .q-pa-md {
+    padding: 1rem;
+  }
+  
+  .q-mt-lg {
+    margin-top: 2rem;
+  }
+  </style>
+  
