a4afe570650b4b541b61387162ee2424a3bb26cc
Alexander Stanevko <dardawill@yandex.ru>
2024-01-23 00:17:48 +0300

    wip

--- .github/scripts/notify_telegram_success.sh
+++ .github/scripts/notify_telegram_success.sh
@@ -1,23 +1,3 @@
-# #!/bin/bash
-
-# telegram_bot_token="${TELEGRAM_BOT_TOKEN}"
-# telegram_chat_id="${TELEGRAM_CHAT_ID}"
-# message="${MESSAGE}"
-
-# reply_markup=$(cat <<EOF
-# {
-#   "inline_keyboard": [
-#     [{"text": "Deploy", "callback_data": "do_deploy"}]
-#   ]
-# }
-# EOF
-# )
-
-# curl -s -X POST "https://api.telegram.org/bot${telegram_bot_token}/sendMessage" \
-#      -d chat_id="${telegram_chat_id}" \
-#      -d text="$message" \
-#      -d "reply_markup=${reply_markup}"
-
 #!/bin/bash
 
 telegram_bot_token="${TELEGRAM_BOT_TOKEN}"
@@ -25,9 +5,7 @@ telegram_chat_id="${TELEGRAM_CHAT_ID}"
 message="${MESSAGE}"
 github_ref="${GITHUB_REF}"
 
-# Функция для отправки сообщения в Telegram
 send_telegram_message() {
-    # Проверка на наличие reply_markup и формирование соответствующего параметра
     local reply_markup_param=""
     if [[ -n "$reply_markup" ]]; then
         reply_markup_param="-d reply_markup=${reply_markup}"
@@ -39,12 +17,9 @@ send_telegram_message() {
          ${reply_markup_param}
 }
 
-# Определение, является ли ветка DEV
 if [[ "$github_ref" == "refs/heads/DEV" ]]; then
-    # Если ветка DEV, отправить сообщение без кнопки
     send_telegram_message
 else
-    # Для других веток, отправить сообщение с кнопкой
     reply_markup=$(cat <<EOF
     {
       "inline_keyboard": [
