1c8fc5bcdaa01e17706fdc10ed918bbbbb054ed1
Alexander Stanevko <dardawill@yandex.ru>
2024-01-23 09:08:15 +0300

    Add fetch request

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 import TheWelcome from '../components/TheWelcome.vue'
+
+const todoList = ref(null)
+
+const initMethod = async () => {
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  todoList.value = await response.json();
+
+}
+
+onMounted(async () => {
+  await initMethod()
+})
 </script>
 
 <template>
